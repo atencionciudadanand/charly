ed7daca395a18e20422eab15053fbfbc6644c328
Carlos Ivan Ramírez Juárez <Carlos Ivan Ramrez Jurez>
2016-10-11 13:22:30 -0500

    segunda version

--- www/js/addImage.js
+++
@@ -1,7 +0,0 @@
- document.addEventListener('DOMContentLoaded', function () { 
-	
-	document.getElementById('image').style.display = 'none';
-	document.getElementById('thumb').style.display = 'none';
-	document.getElementById('video').style.display = 'none';
-	
- }
