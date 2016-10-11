7cd70c13913c6a9a83aa2f9d2ca7f41642f02597
Carlos Ivan Ramírez Juárez <Carlos Ivan Ramrez Jurez>
2016-10-11 11:55:20 -0500

    Primer version

---
+++ www/js/addImage.js
@@ -0,0 +1,7 @@
+ document.addEventListener('DOMContentLoaded', function () { 
+	
+	document.getElementById('image').style.display = 'none';
+	document.getElementById('thumb').style.display = 'none';
+	document.getElementById('video').style.display = 'none';
+	
+ }
