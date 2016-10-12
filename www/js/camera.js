066779ba33a1c57d0182561654751a9a91674556
Carlos Ivan Ramírez Juárez <Carlos Ivan Ramrez Jurez>
2016-10-12 12:41:24 -0500

    prueba de camara

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,32 +1,5 @@
 // JavaScript Document
 $(document).ready(function(e) {
-	
-	/**$("#btnGetCamara").click(function(e){
-		alert(“btnGetCamara”);
-		e.stopPropagation();
-		navigator.camera.getPicture(cameraSuccess, cameraError, {
-			quality: 50,
-			destinationType : Camera.DestinationType.FILE_URI,
-			sourceType: Camera.PictureSourceType.CAMERA,
-			allowEdit: true,
-			encodingType: Camera.EncodingType.JPEG,
-			saveToPhotoAlbum: true		
-		});
-	}); 
-	
-	$("#btnGetLibrary").click(function(e){
-		e.stopPropagation();
-		alert(“btnGetLibrary”);
-		navigator.camera.getPicture(cameraSuccess, cameraError, {
-			quality:50,
-			destinationType : Camera.DestinationType.FILE_URI,
-			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-			allowEdit: true,
-			encodingType: Camera.EncodingType.JPEG,
-			saveToPhotoAlbum: true			
-		});
-	});**/
-	
 	$("#btnGetCamara").click(function(e){
 		alert("puta prueba de mierda");
 		navigator.camera.getPicture(cameraSuccess, cameraError, {
@@ -50,3 +23,4 @@ function cameraSuccess(imageURL){
 function cameraError(msg){
 	navigator.notification.alert("Error capturando foto: " + msg)
 }
+
