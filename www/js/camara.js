0c8d7989524f74ebe89dba380c41054d9c4483db
Carlos Ivan Ramírez Juárez <Carlos Ivan Ramrez Jurez>
2016-10-12 16:28:46 -0500

    prueba de camara

--- www/js/camara.js
+++ www/js/camara.js
@@ -1,43 +1,39 @@
 var app = {
  
-          initialize: function() {
-              this.bindEvents();
-          },
+    initialize: function() {
+		this.bindEvents();
+    },
          
-          bindEvents: function() {
-              var takePhoto = document.getElementById('takePhoto');
-              takePhoto.addEventListener('click', app.takePhoto, false);
-              var sendPhoto = document.getElementById('sendPhoto');
-              sendPhoto.addEventListener('click', app.sendPhoto, false);
-          },
+    bindEvents: function() {
+		var takePhoto = document.getElementById('takePhoto');
+        takePhoto.addEventListener('click', app.takePhoto, false);
+        var sendPhoto = document.getElementById('sendPhoto');
+        sendPhoto.addEventListener('click', app.sendPhoto, false);
+	},
  
-          sendPhoto: function() {
-              alert('Imagen enviada al servidor');
-          },
+    sendPhoto: function() {
+		alert('Imagen enviada al servidor');
+    },
  
-          takePhoto: function(){
-			  alert("lll");
-              navigator.camera.getPicture(app.onPhotoDataSuccess, app.onFail, { quality: 20, 
-                  allowEdit: true, destinationType: navigator.camera.DestinationType.DATA_URL });
-          },
+    takePhoto: function(){
+		navigator.camera.getPicture(app.onPhotoDataSuccess, app.onFail, { 
+			quality: 20, 
+            allowEdit: true, 
+			destinationType: navigator.camera.DestinationType.DATA_URL 
+		});
+	},
  
-          onPhotoDataSuccess: function(imageData) {
-         
-		 	var photo = document.getElementById('photo');
-			var photo2 = document.getElementById('fotoEdit_img');
-			
-			photo2.style.display = 'none';
-            photo.style.display = 'block';
+    onPhotoDataSuccess: function(imageData) {
+		var photo = document.getElementById('photo');
+		var photo2 = document.getElementById('fotoEdit_img');
+		photo2.style.display = 'none';
+		photo.style.display = 'block';
+		photo.src = "data:image/jpeg;base64," + imageData;
+		var sendPhoto = document.getElementById('sendPhoto');
+		sendPhoto.style.display = 'block';
+    },
  
-            photo.src = "data:image/jpeg;base64," + imageData;
- 
-            var sendPhoto = document.getElementById('sendPhoto');
-            sendPhoto.style.display = 'block';
-            
-          },
- 
-          onFail: function(message) {
-            alert('Failed because: ' + message);
-          }
- 
-      };
+    onFail: function(message) {
+		alert('Failed because: ' + message);
+	}
+};
