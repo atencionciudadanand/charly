85e755ccc729738248fce661780a47a583991a35
Carlos Ivan Ramírez Juárez <Carlos Ivan Ramrez Jurez>
2016-10-12 11:47:07 -0500

    prueba de camara

--- www/js/camera.js
+++ www/js/camera.js
@@ -29,6 +29,14 @@ $(document).ready(function(e) {
 	
 	$("#btnGetCamara").click(function(e){
 		alert("puta prueba de mierda");
+		navigator.camera.getPicture(cameraSuccess, cameraError, {
+			quality:50,
+			destinationType : Camera.DestinationType.FILE_URI,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			saveToPhotoAlbum: true			
+		});
 	});
 	
 });
